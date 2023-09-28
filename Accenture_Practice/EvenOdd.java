00c56548cf1075a2bede995ea9f9574b59033464
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2023-09-28 13:49:02 +0530

    Formatted 'EvenOdd.java'

--- Accenture_Practice/EvenOdd.java
+++ Accenture_Practice/EvenOdd.java
@@ -2,14 +2,15 @@ package Accenture_Practice;
 
 public class EvenOdd {
     public static boolean isEven(int num) {
-            if (num%2==0) {
-                return true;
-            } else {
-                return false;
-            }
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
     public static void main(String[] args) {
-        int num=11;
+        int num = 11;
 
         System.out.println(isEven(num));
     }
