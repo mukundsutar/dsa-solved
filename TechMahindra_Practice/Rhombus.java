9463e199c5d54ed0b712f80c7cef53ec6b3c4e86
Mukund Sutar <87423576+mukundsutar@users.noreply.github.com>
2023-11-25 23:59:22 +0530

    Rhombus

---
+++ TechMahindra_Practice/Rhombus.java
@@ -0,0 +1,27 @@
+package TechMahindra_Practice;
+
+public class Rhombus {
+    public static void main(String[] args) {
+        int len = 4;
+
+        for (int i = 1; i <= len; i++) {
+            for (int j = len; j >= i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= len; j++) {
+                if (i == 1 || i == len) {
+                    System.out.print("* ");
+                } else {
+                    if (j == 1 || j == len) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
